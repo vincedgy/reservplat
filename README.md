89ee83608156a42d6aca6f640fd7da15a265f721
Vincent <vincent.dagoury@gmail.com>
2017-05-25 11:29:13 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Microservices with SpringBoot, SpringCloud etc..
+
+The goal : 
+Build a reservation platform using microservices : Sprinboot REST API, JPA, Web (MVC), H2 db, HATEAOS
+
+But also a whole bunch of great tehcnologiy from SpringCloud and Netflix microservices stack (quite standards now)
+- Centralized Configuration server (wich is a microservice itself)
+- Eureka integration for service discovery
+
+## References 
+
+- https://start.spring.io/
+
+- https://www.youtube.com/watch?v=rqQOSG0DWPY
+
+
+## Configuration and choices
+
+
+
+
--- reservation-service/src/main/java/net/vincedgy/reservplat/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/net/vincedgy/reservplat/reservationservice/ReservationServiceApplication.java
@@ -1,7 +1,20 @@
 package net.vincedgy.reservplat.reservationservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
@@ -9,4 +22,70 @@ public class ReservationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
 	}
+
+}
+
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findAll();
+    List<Reservation> findById(Long id);
+}
+
+
+@Component
+class ReservationDataCLR implements CommandLineRunner {
+
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationDataCLR(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        Stream.of("Spencer", "Vincent", "Catherine", "Louis-Philippe", "Emmanuel", "François", "Jacques")
+                .forEach( n -> reservationRepository.save(new Reservation(n)));
+
+        reservationRepository.findAll().forEach(System.out::println);
+    }
+}
+
+@Entity
+class Reservation {
+	public Reservation() {} // JPA
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String reservationName;
+
+    public Reservation(String n) {
+        this.reservationName=n;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "id=" + id +
+                ", reservationName='" + reservationName + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReservationName() {
+        return reservationName;
+    }
+
+    public void setReservationName(String reservationName) {
+        this.reservationName = reservationName;
+    }
 }
