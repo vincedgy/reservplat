088ac29c72663494659d6a8d6f5d3e7419b4f691
Vincent <vincent.dagoury@gmail.com>
2017-05-26 08:02:24 +0200

    Updating files

---
+++ hystrix-dashboard/src/main/java/net/vincedgy/reservplat/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,12 @@
+package net.vincedgy.reservplat.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
