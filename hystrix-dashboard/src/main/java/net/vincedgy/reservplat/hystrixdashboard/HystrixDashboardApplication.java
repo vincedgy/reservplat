ed558e23a24d3312207678c11ff91993f36df441
Vincent <vincent.dagoury@gmail.com>
2017-05-26 08:34:56 +0200

    Updating files

--- hystrix-dashboard/src/main/java/net/vincedgy/reservplat/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/net/vincedgy/reservplat/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,7 +2,9 @@ package net.vincedgy.reservplat.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard
 @SpringBootApplication
 public class HystrixDashboardApplication {
 
